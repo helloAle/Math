d6281b3bf69217f7666b9e460fbb9b670012e309
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2022-09-03 20:39:45 -0300

    Add files via upload

---
+++ lista/lista1ex6.m
@@ -0,0 +1,39 @@
+
+%A altura máxima h alcançada
+%por um objeto arremessado
+%com uma velocidade e em um
+%ângulo 0 em relação à horizontal,
+%desconsiderando a resistência do ar, é:
+
+%Crie uma tabela que mostra a altura
+%máxima para os seguintes valores
+%de vie 0: v 10 , 12 , 14 , 16 , 18 , 20m/s
+%0=50°, 60°, 70⁰ ,80°
+%As linhas na tabela devem corresponder
+%aos valores de velocidade,
+%e as colunas devem corresponder aos ângulos .
+g=9.807
+"NUMERO 1"
+"///"
+v=10
+t=50
+h=((v^2)*((sin(t))^2))/2*g
+"///"
+"NUMERO 2"
+"///"
+v=12
+t=60
+h=((v^2)*((sin(t))^2))/2*g
+"///"
+"NUMERO 3"
+"///"
+v=14
+t=70
+h=((v^2)*((sin(t))^2))/2*g
+"///"
+"NUMERO 4"
+"///"
+v=16
+t=80
+h=((v^2)*((sin(t))^2))/2*g
+
